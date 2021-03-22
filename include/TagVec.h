f5894a4e68c935f154b673fca9acdcbc7c3b2739
Jialong Weng <wngjia001@myuct.ac.za>
2021-03-22 22:33:37 +0200

    Add header files

---
+++ include/TagVec.h
@@ -0,0 +1,18 @@
+/**
+* TagVec.h 
+* CSC3022F Assignment 1 2021
+* Author: WNGJIA001
+*/
+
+# include <string>
+# include <vector>
+# include "Tag.h"
+
+#ifndef _TAGVEC
+#define _TAGVEC
+
+namespace WNGJIA001 {
+    std::vector<Tag> TagVec;
+}
+
+#endif
