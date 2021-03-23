6ddf03ed8f2162ee24fd72aedfc28ac3f031b8ab
Jialong Weng <wngjia001@myuct.ac.za>
2021-03-23 16:31:12 +0200

    Add Menu.h header file

---
+++ include/Menu.h
@@ -0,0 +1,17 @@
+/**
+* Menu.h 
+* CSC3022F Assignment 1 2021
+* Author: WNGJIA001
+*/
+#ifndef _MENU
+#define _MENU
+
+#include <string>
+
+
+namespace WNGJIA001 {
+    void welcome();
+    void displayMenu();
+}
+
+#endif
