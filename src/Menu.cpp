b5322aa4f65bd0a82723654f35bd09d510be6a9c
Jialong Weng <wngjia001@myuct.ac.za>
2021-03-25 00:29:57 +0200

    Add menu messages and methods

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,20 +1,36 @@
 /**
-* Menu.cpp to display text menu
+* Menu.cpp to display menu to the output console
 * CSC3022F Assignment 1 2021
 * Author: WNGJIA001
 */
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "Tags.h"
 
 
 namespace WNGJIA001 {
     void welcome() {
-        std::cout << "Welcome to XML Text Parser" << std::endl;
+        // welcome message
+        std::cout << "++++++++++ Welcome to XML Text Parser ++++++++++" << std::endl << std::endl;
     }
-    void displayMenu(){
-        std::cout << "Please select " << std::endl;
-        std::cout << "XML Text Parser" << std::endl;
+    void toContinue() {
+        // to continue or to quit
+        // displayed after performing an action or the welcome message
+        std::cout << '\n' << "Please enter 'c' to continue or 'q' to quit." << std::endl;
     }
+    void displayMenu() {
+        // display options of the menu
+        std::cout << "Please select an option from below by entering the corresponding letter:" << std::endl << std::endl;
+        std::cout << "r - read and parse an input file" << std::endl;
+        std::cout << "p - print all tags" << std::endl;
+        std::cout << "d - write tags and data to an output file" << std::endl;
+        std::cout << "l - list data for a specific tag" << std::endl;
+        std::cout << "q - quit the program" << std::endl;
+    }
+    void clearTerm(void) {
+        // clear the terminal window
+        system("clear");
+    } 
 }
