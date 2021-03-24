e17f3452dc76b4391676a5186a1d39e1b1b62515
Jialong Weng <wngjia001@myuct.ac.za>
2021-03-25 00:29:17 +0200

    Implement text menu flow

--- src/TagDriver.cpp
+++ src/TagDriver.cpp
@@ -5,12 +5,57 @@
 */
 
 #include <iostream>
-#include "Tag.h"
+#include <string>
+#include <cstdlib>
+#include "Tags.h"
+#include "Menu.h"
 
-
-using namespace WNGJIA001
 
 int main(int argc, char* argv[])
 {
-    welcome();
+    using namespace WNGJIA001;
+    // using WNGJIA001::TagVector;
+    
+    // scope variable declaration
+    std::string option;
+    std::string filename;
+    std::string tagname;
+
+    clearTerm();
+    welcome(); 
+    toContinue();
+    for(;;) { //infinite loop
+        std::cin >> option;
+        clearTerm();
+        if (option == "c") { // continue to menu
+            displayMenu();
+            std::cin >> option;
+            clearTerm();
+            if (option == "r") { // read and parse a specied input file
+                std::cout << "Enter the name of the input file (including .txt): " << std::endl;
+                std::cin >> filename;
+                readTagsFrom(filename);
+            } else if (option == "p") { // print all tags
+                printTags();
+            } else if (option == "d") { // dump/write tags and data to a file called tag.txt
+                std::cout << "Enter a name for the output file (including .txt): " << std::endl;
+                std::cin >> filename;
+                dumpTagsTo(filename);
+            } else if (option == "l") { // list/print tag data for given tag
+                std::cout << "Enter the name of the tag (case sensitive): " << std::endl;
+                std::cin >> tagname;
+                listTag(tagname);
+            } else if (option == "q") { // quit
+                exit(0);
+            } else { // other inputs
+                std::cout << "Not an option!" << std::endl;
+            }
+        } else if (option == "q") { // quit
+                exit(0);
+        } else { // other invalid option
+                std::cout << "Invalid option!" << std::endl;  
+        }
+        toContinue();
+    }
+    return 0;
 }
